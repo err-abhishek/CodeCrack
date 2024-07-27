ea6e1f65309e48e59ad93deafeea6fe311b3de77
Abhishek Agrahari <abhishek737333@gmail.com>
2024-07-27 09:57:12 +0530

    Make Changes in Navbar CSS

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,8 +8,12 @@ const UserNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="mb-4" fixed="top">
       <Container>
-        <Navbar.Brand as={Link} to="/"> 
-          <Link href='/' className=""><h3>&lt;Code/Crack&gt;</h3></Link>
+        <Navbar.Brand as={Link} to="/" >
+          <Link href="/" style={{ textDecoration: "none"}}>
+          <h3 style={{  color: "white" }}>
+      &lt;Code/Crack&gt;
+    </h3>
+          </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
